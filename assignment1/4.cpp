26b1c70537e8e70c697908dcd42973a0c37c3179
vishalkumar9 <vishu926k@gmail.com>
2020-08-24 10:50:17 +0530

    Caesar Cypher Algorithm

--- assignment1/4.cpp
+++ assignment1/4.cpp
@@ -1,3 +1,9 @@
+/*
+Write a C/C++ program to encrypt and decrypt the string using Caesar Cypher Algorithm. While
+encrypting the given string, 5 is added to the ASCII value of the characters. Similarly, for decrypting the
+string, 5 is subtracted from the ASCII value of the characters to print an original string. Example - Input
+String - hello, Encrypted String - mjqqt
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
