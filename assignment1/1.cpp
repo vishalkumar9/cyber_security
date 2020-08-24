7299be30b386b5101d912b22c9321067629a2e69
vishalkumar9 <vishu926k@gmail.com>
2020-08-24 10:47:40 +0530

    command line arguments

--- assignment1/1.cpp
+++ assignment1/1.cpp
@@ -1,3 +1,7 @@
+/*
+  Write a C/C++ program that takes n number of command line arguments and finds the least number.
+In case of invalid entered value, prompt the user to enter another value.  
+ */   
 #include<iostream>
 using namespace std;
 int main(int argc,char** argv){
