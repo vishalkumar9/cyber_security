d7f8fbc46b7d152294766a35b5ce8301f14e2f92
vishalkumar9 <vishu926k@gmail.com>
2020-08-30 17:00:04 +0530

    number of upper case letters and  lower case letters.

---
+++ assignmentpython1/Q3.py
@@ -0,0 +1,18 @@
+# Write a Python function that accepts a string and calculates the number of upper case letters and
+# lower case letters. You can use functions .isupper() and .islower().
+
+def string_test(s):
+    d={"UPPER_CASE":0, "LOWER_CASE":0}
+    for c in s:
+        if c.isupper():
+           d["UPPER_CASE"]+=1
+        elif c.islower():
+           d["LOWER_CASE"]+=1
+        else:
+           pass
+    print ("Original String : ", s)
+    print ("No. of Upper case characters : ", d["UPPER_CASE"])
+    print ("No. of Lower case Characters : ", d["LOWER_CASE"])
+
+str = input("enter your string")
+string_test(str)
