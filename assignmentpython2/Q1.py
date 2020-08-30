491a7017e5ab3ce4bebe4adb09fd0ee8d9b45003
vishalkumar9 <vishu926k@gmail.com>
2020-08-30 16:41:55 +0530

    string is pangram or not

--- assignmentpython2/Q1.py
+++ assignmentpython2/Q1.py
@@ -1,5 +1,8 @@
-Write a function that computes the volume of a sphere given its radius.
+Write a Python function to check whether a string is pangram or not.
+Note: Pangrams are words or sentences containing every letter of the alphabet at least once.
+For example: "The quick brown fox jumps over the lazy dog”
 
+    
 import string, sys
 def ispangram(str1, alphabet=string.ascii_lowercase):
     alphaset = set(alphabet)
