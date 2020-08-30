4c838c746d86cf4e2d3e0bc459a0009d21e2e5a7
vishalkumar9 <vishu926k@gmail.com>
2020-08-30 16:58:11 +0530

    volume of a sphere

---
+++ assignmentpython1/Q1.py
@@ -0,0 +1,6 @@
+#Write a function that computes the volume of a sphere given its radius.
+
+pi=22/7
+radius = input("Radius of sphere: "))
+sur_area = 4 * pi * radius**2
+print("Surface Area is: ", sur_area)
